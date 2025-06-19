68cc03568b3e1a2eea64d157124b22ce4d014fd3
VitorApolonio <vitorapolonio74@gmail.com>
2025-06-19 12:37:40 -0300

    replace point-based drawing algorithm with a line-based one
    The way drawing was implemented resulted in gaps being left when the mouse
    pointer moved too quickly.
    
    By drawing lines between different mouse positions, fast movement simply
    results in straight lines instead.

--- src/render.js
+++ src/render.js
@@ -1,31 +1,51 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
+// positions to begin line
+const startPos = { x: 0, y: 0 }
+
 let mouseClicked = false
 
+// brush settings
 let brushSize = 5
 let brushColor = '#ffffff'
 
-document.addEventListener('mousedown', () => {
+// set line start position on mouse click
+canvas.addEventListener('mousedown', e => {
   mouseClicked = true
+  startPos.x = e.x - canvas.offsetLeft
+  startPos.y = e.y - canvas.offsetTop
 })
 
-document.addEventListener('mouseup', () => {
+// stop drawing when mouse is released
+canvas.addEventListener('mouseup', () => {
   mouseClicked = false
 })
 
+// draw line from startPos to current mouse position, then set startPos to current
 canvas.addEventListener('mousemove', e => {
   if (mouseClicked) {
-    ctx.beginPath()
-    ctx.fillStyle = brushColor
-    ctx.ellipse(e.x - canvas.offsetLeft, e.y - canvas.offsetTop, brushSize, brushSize, 0, 0, Math.PI * 2)
-    ctx.fill()
+    draw(e)
+    startPos.x = e.x - canvas.offsetLeft
+    startPos.y = e.y - canvas.offsetTop
   }
 })
 
+// draw a dot on mouse click
 canvas.addEventListener('mouseup', e => {
   ctx.beginPath()
   ctx.fillStyle = brushColor
-  ctx.ellipse(e.x - canvas.offsetLeft, e.y - canvas.offsetTop, brushSize, brushSize, 0, 0, Math.PI * 2)
+  ctx.ellipse(e.x - canvas.offsetLeft, e.y - canvas.offsetTop, brushSize / 2, brushSize / 2, 0, 0, Math.PI * 2)
   ctx.fill()
 })
+
+// drawing algorithm
+const draw = (e) => {
+  ctx.beginPath()
+  ctx.lineCap = 'round'
+  ctx.lineWidth = brushSize
+  ctx.strokeStyle = brushColor
+  ctx.moveTo(startPos.x, startPos.y)
+  ctx.lineTo(e.x - canvas.offsetLeft, e.y - canvas.offsetTop)
+  ctx.stroke()
+}
