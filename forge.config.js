487ffdf5d65841f8857c35405e79f3376b182132
VitorApolonio <vitorapolonio74@gmail.com>
2025-06-28 18:09:24 -0300

    allow publishing releases with GitHub Actions

--- forge.config.js
+++ forge.config.js
@@ -4,48 +4,49 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: 'src/img/icon'
+    icon: 'src/img/icon',
   },
   rebuildConfig: {},
   makers: [
+    // Windows
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+      name: '@electron-forge/maker-nsis',
+      config: {
+        oneClick: false,
+        allowToChangeInstallationDirectory: true,
+        createDesktopShortcut: true,
+        installerIcon: 'src/img/icon.ico',
+        setupIcon: 'src/img/icon.ico',
+      },
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
+    // Linux
     {
       name: '@electron-forge/maker-deb',
       config: {
         options: {
-          icon: 'src/img/icon.png'
+          bin: 'ePaint',
+          icon: 'src/img/icon.png',
         }
       },
     },
+    // macOS
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
     },
   ],
   plugins: [
     {
       name: '@electron-forge/plugin-vite',
       config: {
-        // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-        // If you are familiar with Vite configuration, it will look really familiar.
         build: [
           {
-            // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
             entry: 'src/main.js',
             config: 'vite.main.config.mjs',
-            target: 'main',
           },
           {
             entry: 'src/preload.js',
             config: 'vite.preload.config.mjs',
-            target: 'preload',
           },
         ],
         renderer: [
@@ -56,8 +57,6 @@ module.exports = {
         ],
       },
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
