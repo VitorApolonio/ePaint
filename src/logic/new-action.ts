f2b7f38fa570ccd223102183f45aceaf8304e953
VitorApolonio <vitorapolonio74@gmail.com>
2025-07-01 22:13:38 -0300

    start refactoring action.js
    The new version takes advantage of TypeScript's interfaces, separating
    actions into different types while still sharing a common base between
    them.

---
+++ src/logic/new-action.ts
@@ -0,0 +1,90 @@
+import Brush from "./brush";
+
+/** Represents a position on the canvas, with x- and y-coordinates. */
+interface Position { x: number, y: number }
+
+/**
+ * Represents an operation that changes the state of the canvas in some way.
+ *
+ * An Action is essentially anything that can be undone/redone by the
+ * undo/redo buttons, respectively, such as a path drawn or a region filled.
+ */
+interface Action {
+  /** Executes the action, changing the canvas state as necessary. */
+  perform(brush: Brush): void;
+}
+
+/**
+ * Represents a fill operation.
+ */
+class FillAction implements Action {
+  #fillData: ImageData;
+
+  /**
+   * Creates a FillAction instance.
+   * @param {ImageData} fillData - The state of the canvas after the fill.
+   */
+  constructor(fillData: ImageData) {
+    this.#fillData = fillData;
+  }
+
+  perform(brush: Brush) {
+    brush.drawImage(this.#fillData);
+  }
+}
+
+/**
+ * Represents a path or single point drawn on the canvas.
+ */
+class DrawAction implements Action {
+  #positions: Position[];
+  #brushSize: number;
+  #brushColor: string;
+
+  /**
+   * Creates a DrawAction instance.
+   * @param {number} brushSize - The current brush size (px)
+   * @param {string} brushColor - The current brush color (hex)
+   * @param {[Position, ...Position[]]} positions - The positions composing the path
+   */
+  constructor(brushSize: number, brushColor: string, positions: [Position, ...Position[]]) {
+    this.#positions = positions;
+    this.#brushSize = brushSize;
+    this.#brushColor = brushColor;
+  }
+
+  perform(brush: Brush) {
+    // set brush state
+    brush.size = this.#brushSize;
+    brush.color = this.#brushColor;
+
+    let startPos = this.#positions[0];
+    // draw single point if array has only one position
+    if (this.#positions.length === 1) {
+      brush.drawPoint(startPos.x, startPos.y);
+    // draw path otherwise
+    } else {
+      for (let i = 1; i < this.#positions.length; i++) {
+        const nextPos = this.#positions[i];
+        brush.drawLine(startPos.x, startPos.y, nextPos.x, nextPos.y);
+        startPos = nextPos;
+      }
+    }
+  }
+}
+
+/**
+ * Represents the action of clearing the canvas.
+ */
+class ClearAction implements Action {
+  /**
+   * Create a ClearAction instance.
+   */
+  constructor() {}
+
+  perform(brush: Brush) {
+    brush.clearCanvas();
+  }
+}
+
+export { Action, FillAction, DrawAction, ClearAction };
